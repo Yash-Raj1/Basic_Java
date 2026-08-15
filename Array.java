0d4d385aa7f4b4c189d121004aaccc3717c5af17
yashraj89360@gmail.com <nikhilraj89360@gmail.com>
2026-08-15 17:53:15 +0530

    update

--- Array.java
+++ Array.java
@@ -1,11 +1,16 @@
 class Array{
     public static void main(String[] args){
         int multi[][] = new int [3][4];
-
+        
+        for( int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                multi[i][j] = (int) (Math.random() * 10);
+            }
+        }
 
         for( int i=0; i<3; i++){
             for(int j=0; j<4; j++){
-                System.out.print("Enter the value for multi["+i+"]["+j+"] : " + multi[i][j]);
+                System.out.print(  multi[i][j] + " ");
             }
             System.out.println();
         }
