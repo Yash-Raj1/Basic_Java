ce735d48e8f57fb45a0b01f2e737e9fe41e0b164
yashraj89360@gmail.com <nikhilraj89360@gmail.com>
2026-08-16 18:35:52 +0530

    String Basic files

--- String1.java
+++ String1.java
@@ -7,6 +7,11 @@ public class String1 {
         System.out.println(name.hashCode());
         System.out.println(name.concat(" is a good boy"));
         System.out.println(name.length());
-        System.out.println(name.charAt(11));
+        System.out.println(name.charAt(1));
+    
+
+        String n1="Rishu";
+        String n2 = "Rishu";
+        System.out.println(n1==n2);;
     }
 }
