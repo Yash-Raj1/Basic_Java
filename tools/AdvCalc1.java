c2cfe41977dffabd3d3c7945cc5b02d80ba98f9d
yashraj89360@gmail.com <nikhilraj89360@gmail.com>
2026-08-23 19:08:06 +0530

    update

---
+++ tools/AdvCalc1.java
@@ -0,0 +1,11 @@
+package tools;
+
+public class AdvCalc1 extends Calc1{
+    public int mul(int n1,int n2){
+        return n1*n2;
+    }
+
+    public int div(int n1,int n2){
+        return n1/n2;
+    }
+}
