0d1fe15f59850f60826fde64d3b4982c3a711740
yashraj89360@gmail.com <nikhilraj89360@gmail.com>
2026-08-25 17:25:07 +0530

    Understanding the use of Super Keyword

--- Super.java
+++ Super.java
@@ -24,6 +24,6 @@ public class Super{
     public static void main(String [] args){
        //B obj = new B(); //Calls the default constructor of B and super call the default constructor of super class i.e, A but also if super method is taking integer as parameter then it will call that constructor of super class which is accepting the parameter (parameterized constructor)
         B obj =new B(5); //calls the parameterized constructor of B but firstly call the constructor of super class i.e, A
-
+        System.out.println(obj);
     }
 }
